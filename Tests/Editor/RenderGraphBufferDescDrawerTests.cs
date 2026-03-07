8b029f85ae9dfd1f853ca7923e972911c1bd0860
af8a2a <1125209282@qq.com>
2026-03-07 09:06:59 +0800

    Add RenderGraphBufferDesc drawer

---
+++ Tests/Editor/RenderGraphBufferDescDrawerTests.cs
@@ -0,0 +1,50 @@
+using NUnit.Framework;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+using VividRP.Editor.RenderGraph;
+using VividRP.Runtime;
+
+namespace VividRP.Editor.Tests
+{
+    public class RenderGraphBufferDescDrawerTests
+    {
+        private sealed class RenderGraphBufferDescHost : ScriptableObject
+        {
+            [SerializeField]
+            private RenderGraphBufferDesc m_Descriptor = new RenderGraphBufferDesc();
+        }
+
+        [Test]
+        public void CreatePropertyGUI_BuildsEditableBufferDescriptorFoldoutSections()
+        {
+            var host = ScriptableObject.CreateInstance<RenderGraphBufferDescHost>();
+
+            try
+            {
+                var serializedObject = new SerializedObject(host);
+                var property = serializedObject.FindProperty("m_Descriptor");
+                var drawer = new RenderGraphBufferDescDrawer();
+
+                var root = drawer.CreatePropertyGUI(property);
+                var layoutFoldout = root.Q<Foldout>("vivid-buffer-desc-section-layout");
+                var usageFoldout = root.Q<Foldout>("vivid-buffer-desc-section-usage");
+                var metadataFoldout = root.Q<Foldout>("vivid-buffer-desc-section-metadata");
+
+                Assert.That(root, Is.TypeOf<Foldout>());
+                Assert.That(layoutFoldout, Is.Not.Null);
+                Assert.That(usageFoldout, Is.Not.Null);
+                Assert.That(metadataFoldout, Is.Not.Null);
+                Assert.That(layoutFoldout.Q<PropertyField>("vivid-buffer-desc-field-Count"), Is.Not.Null);
+                Assert.That(layoutFoldout.Q<PropertyField>("vivid-buffer-desc-field-Stride"), Is.Not.Null);
+                Assert.That(usageFoldout.Q<PropertyField>("vivid-buffer-desc-field-Target"), Is.Not.Null);
+                Assert.That(metadataFoldout.Q<PropertyField>("vivid-buffer-desc-field-Name"), Is.Not.Null);
+            }
+            finally
+            {
+                Object.DestroyImmediate(host);
+            }
+        }
+    }
+}

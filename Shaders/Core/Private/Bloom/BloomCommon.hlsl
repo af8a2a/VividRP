3beba61493e488d5cee1dc39b226dee83da8e5b1
af8a2a <1125209282@qq.com>
2026-07-04 09:13:14 +0800

    Use half precision in bloom shaders

--- Shaders/Core/Private/Bloom/BloomCommon.hlsl
+++ Shaders/Core/Private/Bloom/BloomCommon.hlsl
@@ -14,4 +14,15 @@ float3 QuadraticThreshold(float3 color, float threshold, float3 curve)
     return color;
 }
 
+half3 QuadraticThresholdHalf(half3 color, half threshold, half3 curve)
+{
+    half br = max(max(color.r, color.g), color.b);
+
+    half rq = clamp(br - curve.x, half(0.0), curve.y);
+    rq = curve.z * rq * rq;
+
+    color *= max(rq, br - threshold) / max(br, half(1e-4));
+    return color;
+}
+
 #endif // VIVID_BLOOM_COMMON
